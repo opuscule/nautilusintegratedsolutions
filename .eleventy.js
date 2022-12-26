1469b2de25f3e6464844299371182e1637e54f2b
Robert C. Bowman III <bowman@opuscule.com>
2022-12-26 15:16:56 -0500

    Initial Commit

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy('./src/images');
+	eleventyConfig.addPassthroughCopy('./src/js');
+	eleventyConfig.addPassthroughCopy('./src/styles/styles.css');
+	//eleventyConfig.addPassthroughCopy('./src/resource');
+	eleventyConfig.addPassthroughCopy('./src/_redirects');
+	//eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+	return {
+		dir: {
+			input:"src",
+			output:"public"
+		},
+		passthroughFileCopy: true
+	};	
+}
