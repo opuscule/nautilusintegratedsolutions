11e78e8dfc5c0b876ce8204359bdc797685508a6
Robert C. Bowman III <bowman@opuscule.com>
2023-01-04 10:57:00 -0500

    Minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
+const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/images');
 	eleventyConfig.addPassthroughCopy('./src/js');
 	eleventyConfig.addPassthroughCopy('./src/styles/styles.css');
-	//eleventyConfig.addPassthroughCopy('./src/resource');
 	eleventyConfig.addPassthroughCopy('./src/_redirects');
-	//eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+	eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
 	return {
 		dir: {
 			input:"src",
